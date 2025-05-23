4278b0979422c66b96ef6e226de165394c216096
Rodrigo Otreras <rodriotreras@gmail.com>
2025-05-23 13:58:48 -0300

    Se agrego Tilwindcss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 function Home() {
   return (
     <div>
-      <h1>Hola, Soy Rodrigo Otreras</h1>
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hola, soy Rodrigo Otreras
+      </h1>
       <p>Bienvenido a mi portfolio web 🚀</p>
     </div>
   );
