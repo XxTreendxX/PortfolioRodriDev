e079d75db4f837ef207b26430d243f3a6397386d
Rodrigo Otreras <rodriotreras@gmail.com>
2025-06-02 10:02:26 -0300

    Se agregop proyecto FestivalMusica

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,20 +1,30 @@
 export const projects = [
   {
     nombre: "Portfolio Web",
-    descripcion: "Mi portfolio profesional, construido con React y Tailwind CSS.",
+    descripcion:
+      "Mi portfolio profesional, construido con React y Tailwind CSS.",
     tecnologias: ["React", "Tailwind CSS"],
     imagen: "/portfolio.jpg",
     demo: "https://www.rodridev.com.ar",
-    github: "https://github.com/XxTreendxX/PortfolioRodriDev"
+    github: "https://github.com/XxTreendxX/PortfolioRodriDev",
   },
   {
     nombre: "Galería Fotográfica - Login y Autenticación",
-    descripcion: "Sistema de inicio de sesión moderno para una galería de fotografía profesional. Combina seguridad, experiencia visual y una interfaz atractiva, ideal como base para proyectos creativos con gestión de usuarios.",
+    descripcion:
+      "Sistema de inicio de sesión moderno para una galería de fotografía profesional. Combina seguridad, experiencia visual y una interfaz atractiva, ideal como base para proyectos creativos con gestión de usuarios.",
     tecnologias: ["React", "Tailwind CSS"],
     imagen: "/Fotografia.jpg",
     demo: "https://galeria-fotografica-login-autenticacion.vercel.app/",
-    github: "https://github.com/XxTreendxX/GaleriaFotografica-LoginAutenticacion"
-  }
+    github:
+      "https://github.com/XxTreendxX/GaleriaFotografica-LoginAutenticacion",
+  },
+  {
+    nombre: "Techno & EDM Festival 🎵",
+    descripcion:
+      "Proyecto web responsive para un festival de música. Incluye LineUp, galería de imágenes interactiva, y sección de boletos. Todo el código está modularizado para fácil mantenimiento y escalabilidad.",
+    tecnologias: ["HTML5", "SCSS (Sass)", "JavaScript", "Node.js", "Gulp"],
+    imagen: "/portfolio.jpg",
+    demo: "https://www.rodridev.com.ar",
+    github: "https://github.com/XxTreendxX/PortfolioRodriDev",
+  },
 ];
-
-  
