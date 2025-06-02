451b92f5cde9aabe47caa82bc334eefbc041d1dd
Rodrigo Otreras <rodriotreras@gmail.com>
2025-06-02 10:04:46 -0300

    Se actualizacion de proyectos

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     nombre: "Portfolio Web",
     descripcion:
       "Mi portfolio profesional, construido con React y Tailwind CSS.",
-    tecnologias: ["React", "Tailwind CSS"],
+    tecnologias: ["React", "Tailwind CSS", "JavaScript"],
     imagen: "/portfolio.jpg",
     demo: "https://www.rodridev.com.ar",
     github: "https://github.com/XxTreendxX/PortfolioRodriDev",
@@ -12,7 +12,7 @@ export const projects = [
     nombre: "Galería Fotográfica - Login y Autenticación",
     descripcion:
       "Sistema de inicio de sesión moderno para una galería de fotografía profesional. Combina seguridad, experiencia visual y una interfaz atractiva, ideal como base para proyectos creativos con gestión de usuarios.",
-    tecnologias: ["React", "Tailwind CSS"],
+    tecnologias: ["React", "Tailwind CSS", "JavaScript"],
     imagen: "/Fotografia.jpg",
     demo: "https://galeria-fotografica-login-autenticacion.vercel.app/",
     github:
@@ -23,7 +23,7 @@ export const projects = [
     descripcion:
       "Proyecto web responsive para un festival de música. Incluye LineUp, galería de imágenes interactiva, y sección de boletos. Todo el código está modularizado para fácil mantenimiento y escalabilidad.",
     tecnologias: ["HTML5", "SCSS (Sass)", "JavaScript", "Node.js", "Gulp"],
-    imagen: "/portfolio.jpg",
+    imagen: "/FestivalMusica.png",
     demo: "https://www.rodridev.com.ar",
     github: "https://github.com/XxTreendxX/PortfolioRodriDev",
   },
